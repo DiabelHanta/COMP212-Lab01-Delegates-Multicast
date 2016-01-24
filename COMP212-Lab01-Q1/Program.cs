7d257db2aa9d1b89b757a6fc3bca21fea73114e0
DiabelHanta <faim.khandker@gmail.com>
2016-01-24 13:34:25 -0500

    Commit 1 - Initial Commit

---
+++ COMP212-Lab01-Q1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP212_Lab01_Q1
+{
+    class CalculateDelegate
+    {
+        //Global Delegate Variable
+        public delegate void CalculateDel(double num1, double num2);
+
+        //Delegate Methods (Operations)
+        //HELP: Do I make it a VOID or a DOUBLE return?
+        public double Add(double num1, double num2)
+        {
+            double sum = num1 + num2;
+            return sum;
+        }
+
+        public double Subtract(double num1, double num2)
+        {
+            double difference = num1 - num2;
+            return difference;
+        }
+
+        public double Multiply(double num1, double num2)
+        {
+            double product = num1 * num2;
+            return product;
+        }
+
+        public double Divide(double num1, double num2)
+        {
+            double quotient = num1 / num2;
+            return quotient;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Delegate Variables
+            //CalculateDel addDel, subDel, multDel, divDel;
+
+            //Instance Variables
+
+        }
+    }
+}
---
+++ COMP212-Lab01/Driver.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP212_Lab01
+{
+ 
+    class Driver
+    {
+        static void Main(string[] args)
+        {
+            //Delegate Variables
+            //CalculateDel addDel, subDel, multDel, divDel;
+
+            //Instance Variables
+            public double num1 = Convert.ToDouble(Console.ReadLine());
+            public double num2 = Convert.ToDouble(Console.ReadLine());
+
+            //Prompting User
+            //Console.WriteLine("Enter the first number: \nEnter the second number: ", num1, num2);
+
+
+            //Variables pointing to Methods
+            
+            //Delegate Methods (Operations)
+            //HELP: Do I make it a VOID or a DOUBLE return?
+            public double Add(double num1, double num2)
+            {
+                double sum = num1 + num2;
+                return sum;
+            }
+
+            public double Subtract(double num1, double num2)
+            {
+                double difference = num1 - num2;
+                return difference;
+            }
+
+            public double Multiply(double num1, double num2)
+            {
+                double product = num1 * num2;
+                return product;
+            }
+
+            public double Divide(double num1, double num2)
+            {
+                double quotient = num1 / num2;
+                return quotient;
+            }
+        }
+    }
+}
